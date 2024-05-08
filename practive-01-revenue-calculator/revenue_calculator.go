871c04d99ddb7a056198ad8c6a2065169f557794
darryl <darrylestrada97@gmail.com>
2024-05-08 14:57:59 +0200

    chore: Refactor revenue calculation and improve output

--- practive-01-revenue-calculator/revenue_calculator.go
+++ practive-01-revenue-calculator/revenue_calculator.go
@@ -7,18 +7,34 @@ func main() {
 	var expenses float64
 	var taxRate float64
 
-	fmt.Print("Introduce the revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Introduce the expenses: ")
-	fmt.Scan(&expenses)
-	fmt.Print("The tax rate of the revenue calculator: ")
-	fmt.Scan(&taxRate)
+	revenue= scanValue("Introduce the revenue:")
+	expenses= scanValue("Introduce the expenses:")
+	taxRate= scanValue("Introduce the tax rate:")
+	// fmt.Print("Introduce the revenue: ")
+	// fmt.Scan(&revenue)
+	// fmt.Print("Introduce the expenses: ")
+	// fmt.Scan(&expenses)
+	// fmt.Print("The tax rate of the revenue calculator: ")
+	// fmt.Scan(&taxRate)
 
-	earningsBT := revenue - expenses
-	earningsAT := earningsBT * (1-taxRate / 100)
-	ratio := earningsBT / earningsAT
-	fmt.Printf("Earning before taxes: %v\n",earningsBT)
-	fmt.Printf("Earning after taxes: %v\n",earningsAT)
-	fmt.Println("ratio:",ratio)
+	earningsBT, earningsAT, ratio := calculateFutureValues(revenue, expenses, taxRate)
 
+	fmt.Printf("Earning before taxes: %v\n", earningsBT)
+	fmt.Printf("Earning after taxes: %v\n", earningsAT)
+	fmt.Printf("ratio: %.2f", ratio)
+
+}
+func calculateFutureValues(revenue, expenses, taxRate float64) (earningsBT float64, earningsAT float64, ratio float64) {
+	earningsBT = revenue - expenses
+	earningsAT = earningsBT * (1 - taxRate/100)
+	ratio = earningsBT / earningsAT
+	return
+}
+
+//function that get a text and prints the text and scans the value from the user, return the value the user inputs.
+func scanValue(text string) (value float64) {
+
+	fmt.Print(text)
+	fmt.Scan(&value)
+	return value
 }
