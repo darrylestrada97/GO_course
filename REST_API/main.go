0c9967bcb2ad79257c965f174d934cd117cef54b
darryl <darrylestrada97@gmail.com>
2024-05-26 22:46:56 +0200

    Created the create event endpoint

--- REST_API/main.go
+++ REST_API/main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"example/rest_api/models"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/events", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "events",
-		})
-
-	})
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvents)
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -23,4 +22,30 @@ func main() {
 		println("Error: ", err)
 		return
 	}
+}
+
+func createEvents(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "bad_request"})
+
+		fmt.Println(err)
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  "created",
+		"message": event,
+	})
+}
+func getEvents(c *gin.Context) {
+	events := models.GetAllEvents()
+	c.JSON(http.StatusOK, gin.H{
+		"result": events,
+	})
 }
---
+++ REST_API/models/event.go
@@ -0,0 +1,23 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int       `json:"id"`
+	Name        string    `binding:"required" json:"name"`
+	Description string    `binding:"required" json:"description"`
+	Location    string    `binding:"required" json:"location"`
+	DateTime    time.Time `binding:"required" json:"dateTime"`
+	UserID      int       `json:"user_id"`
+}
+
+var events []Event
+
+func (e Event) Save() {
+	// save the event to the database
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
