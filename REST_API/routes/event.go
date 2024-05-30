3743f286c2ebfdd605e261fed8cd37afe1729d00
darryl <darrylestrada97@gmail.com>
2024-05-30 23:49:22 +0200

    created the event.go file

---
+++ REST_API/routes/event.go
@@ -0,0 +1,57 @@
+package routes
+
+import (
+	"example/rest_api/models"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetEvent(c *gin.Context) {
+	id := c.Param("id")
+	event, err := models.GetEvent(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "could not parse id",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, event)
+}
+func CreateEvents(c *gin.Context) {
+	var event models.Event
+	err := c.ShouldBindJSON(&event)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "bad_request"})
+
+		fmt.Println(err)
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "could_not_save_event",
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"status":  "created",
+		"message": event,
+	})
+}
+func GetEvents(c *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal_server_error",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"result": events,
+	})
+}
