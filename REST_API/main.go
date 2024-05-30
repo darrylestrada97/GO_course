08fe40717ed478976e1ef92829386615be4e4c79
darryl <darrylestrada97@gmail.com>
2024-05-30 23:49:44 +0200

    moved the routes registry logic to routes.go

--- REST_API/main.go
+++ REST_API/main.go
@@ -2,76 +2,18 @@ package main
 
 import (
 	"example/rest_api/db"
-	"example/rest_api/models"
+	"example/rest_api/routes"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.GET("/events/:id", getEvent)
-	server.POST("/events", createEvents)
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	routes.RegisterRoutes(server)
 	err := server.Run(":8080")
 	if err != nil {
-		println("Error: ", err)
+		fmt.Println("Error: ", err)
 		return
 	}
-}
-
-func getEvent(c *gin.Context) {
-	id := c.Param("id")
-	event, err := models.GetEvent(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "could not parse id",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, event)
-}
-func createEvents(c *gin.Context) {
-	var event models.Event
-	err := c.ShouldBindJSON(&event)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "bad_request"})
-
-		fmt.Println(err)
-		return
-	}
-	event.ID = 1
-	event.UserID = 1
-
-	err = event.Save()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "could_not_save_event",
-		})
-		return
-	}
-	c.JSON(http.StatusCreated, gin.H{
-		"status":  "created",
-		"message": event,
-	})
-}
-func getEvents(c *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "internal_server_error",
-		})
-		return
-	}
-	c.JSON(http.StatusOK, gin.H{
-		"result": events,
-	})
 }
